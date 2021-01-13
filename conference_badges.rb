afc97616fdbd478ad687c7251fa7fabe33982e19
Reid <rlj0713@gmail.com>
2021-01-13 11:12:14 -0500

    commit tree cef3260d69d781d5ba38df697f882ef6ea0e989d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array2 = []
+    array.each {|element| array2 << badge_maker(element)}
+    return array2
+end
+
+def assign_rooms(speakers)
+    list = []
+        speakers.each {|speaker| list << "Hello, #{speaker}! You'll be assigned to room #{list.length + 1}!"}
+    list
+end
+
+def printer(attendees)
+    i = 0
+    while i < attendees.length  
+        puts (batch_badge_creator(attendees)[i])
+        puts (assign_rooms(attendees)[i])
+        i += 1
+    end
+  end
